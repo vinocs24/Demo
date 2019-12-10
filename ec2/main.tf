d515d21c7e4ce884fe1ca39cb921bebc1f78558e
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-10 19:26:03 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,1 +1,10 @@
+resource "aws_instance" "ec2-instance" {
+    name                        = var.configuration-name
+    image_id                    = var.image-id
+    instance_type               = var.instance-type
+    security_groups             = [var.security-group-id, var.rds-security-group]
+    associate_public_ip_address = true
+    key_name                    = var.key-name
+    user_data                   = file("EC2/installing-components.sh")
+}
 
