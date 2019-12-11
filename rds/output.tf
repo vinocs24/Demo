2792922a94a1630a022ae73e7baea07358e6e150
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-11 15:43:27 +0530

    Update output.tf

--- rds/output.tf
+++ rds/output.tf
@@ -1,1 +1,19 @@
+output "rds_address" {
+  value = aws_db_instance.rds.address
+}
 
+output "terra_db_sg_id" {
+  value = aws_security_group.rds_sg.id
+}
+
+output "rds_user" {
+  value = aws_db_instance.rds.username
+}
+
+output "rds_password" {
+  value = aws_db_instance.rds.password
+}
+
+output "rds_dbname" {
+  value = aws_db_instance.rds.name
+}
