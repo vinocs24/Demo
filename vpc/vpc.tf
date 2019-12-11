9aadfded4da0d8968ff8c18e07cf5b6e355434a9
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-11 12:05:33 +0530

    Update vpc.tf

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -2,11 +2,11 @@ resource "aws_vpc" "terra-vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = "true"
 
-  tags = {
+  tags {
     Name = "terra-vpc"
   }
 }
 
-output "id" {
+output "vpc_id" {
   value = aws_vpc.terra-vpc.id
 }
