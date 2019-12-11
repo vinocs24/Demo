b0ff66b27984bded16d6e699762960fef762d6a8
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-11 12:30:42 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -5,6 +5,6 @@ resource "aws_instance" "ec2-instance" {
     security_groups             = [var.security-group-id, var.rds-security-group]
     associate_public_ip_address = true
     key_name                    = var.key-name
-    user_data                   = file("EC2/installing-components.sh")
+    user_data                   = file("ec2/installing-components.sh")
 }
 
