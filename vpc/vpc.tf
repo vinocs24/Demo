fb9c16c5df727a769d2ecf21202b4f5b531abbbd
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-11 11:58:38 +0530

    Create vpc.tf

---
+++ vpc/vpc.tf
@@ -0,0 +1,12 @@
+resource "aws_vpc" "terra-vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    Name = "terra-vpc"
+  }
+}
+
+output "id" {
+  value = aws_vpc.terra-vpc.id
+}
