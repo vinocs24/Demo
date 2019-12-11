528dfe719eb4c5255ad0dee0d6d5636e6e1bfae5
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-11 15:33:54 +0530

    Update main.tf

--- rds/main.tf
+++ rds/main.tf
@@ -7,8 +7,36 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
   name        = "terra-db-subnet"
   description = "RDS subnet group"
   subnet_ids  = [var.subnet_ids]
-
 }
+
+resource "aws_security_group" "rds_sg.id" {
+  name        = "rds-db-tf"
+  description = "Access to the RDS instances from the VPC"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  ingress {
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_db_instance" "rds" {
     identifier             = "terraform-db"
     allocated_storage      = var.allocated_storage
