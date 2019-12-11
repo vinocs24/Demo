1ebd072d516747910196fe9299b81ba60f59996b
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-11 12:08:33 +0530

    Create subnet.tf

---
+++ vpc/subnet.tf
@@ -0,0 +1,30 @@
+resource "aws_subnet" "terra-public-subnet" {
+    vpc_id     = aws_vpc.demo-vpc.id
+    cidr_block = "10.0.0.0/24"
+    availability_zone = "us-west-2a"
+
+    tags = {
+        Name = "terra-public-subnet"
+    }
+}
+
+resource "aws_subnet" "terra-private-subnet" {
+    vpc_id     = aws_vpc.demo-vpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-west-2b"
+
+    tags {
+        Name = "terra-private-subnet"
+    }
+}
+
+
+output "public-id" {
+  value = aws_subnet.terra-public-subnet.id
+}
+
+output "private-id" {
+  value = aws_subnet.terra-private-subnet.id
+}
+
+
